6e6f07f6e3f4f09342a0980c02258385183f9088
k-cziberez <czibere.zoltan@ulyssys.hu>
2025-08-27 11:14:01 +0200

    nyelvváltás támogatás

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit, signal} from '@angular/core';
+import {Component} from '@angular/core';
 import {QrComponent} from './qr-component/qr-component';
-import {TranslocoService} from '@ngneat/transloco';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +7,5 @@ import {TranslocoService} from '@ngneat/transloco';
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App implements OnInit {
-
-  constructor(private translocoService: TranslocoService) {
-  }
-
-  ngOnInit() {
-    document.documentElement.lang = this.translocoService.getActiveLang();
-    this.translocoService.langChanges$.subscribe(lang => {
-      document.documentElement.lang = lang;
-    });
-  }
-
-  protected readonly title = signal('wifi-qr');
+export class App {
 }
